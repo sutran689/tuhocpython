099060b21d3e01d2cf0ce49900a2f4d9cbf97f4a
sutran0689 <sutran689@gmail.com>
2017-11-25 11:24:37 +0700

    chen bến vô chuỗi

---
+++ EX5.py
@@ -0,0 +1,18 @@
+my_name = 'zed A Shaw'
+my_age = 35
+my_height = 180
+my_weight = 74
+my_eyes = ' Blue'
+my_teeth = 'white'
+my_hair  = 'Browm '
+
+
+print "let's talk about %s." % my_name
+print "he's %d inches tall." % my_height
+print "he's %d pounds heavy." % my_weight
+print " Actually that's not too heavy."
+print "he's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "his teeth are usually %s depenling on the coffee. " % my_teeth
+
+# this line is tricky try to get it exacly right\
+print "it i add %d, %d and i get %d. " % (my_age, my_height, my_weight, my_age + my_height + my_weight)
